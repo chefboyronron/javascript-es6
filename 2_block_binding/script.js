56f624e68b5e5568ba14d5ea74c4660aef2a8e26
chefboyronron <rgpseminiano@gmail.com>
2018-04-28 20:16:18 +0800

    Use for loop to loop an array and object and use const, let and var binding

--- 2_block_binding/script.js
+++ 2_block_binding/script.js
@@ -1,6 +1,6 @@
 /* constant variables */
 const person = {
-    name : 'ron'
+    name : 'ronnn'
 }
 
 function changeName() {
@@ -25,3 +25,61 @@ function getName() {
 getName();
 // error: undefined variable friend
 // console.log(friend);
+
+/* ===============================
+    Let and constant in a loop
+==================================*/
+
+for(var a = 0; a < 10; a++ ) {
+    // console.log(i);
+}
+// variable "a" is available outside the loop
+console.log(a);
+
+for(let b = 0; b < 10; b++ ) {
+    // let variable "b" is only available inside this block
+}
+// variable "b" is not available outside the loop
+// output an error!
+//console.log(b);
+
+/* functions inside the loop */
+funcs = [];
+for (var c = 0; c < 10; c++) {
+    funcs.push(
+        function(loopVal){
+            return function() {
+                console.log(loopVal);
+            }
+        }(c)
+    );
+}
+// display the loop value of c where 0 to 9
+funcs.forEach(function(func) {
+    func();
+});
+
+/* function inside the loop : object */
+var container = [];
+var members = {
+    a : {
+        name : 'ron'
+    },
+    b : {
+        name : 'kyle'
+    },
+    c : {
+        name : 'ryle'
+    }
+}
+// note you can use const to loop an object
+for ( let key in members ) {
+    container.push(
+        function() {
+            console.log(members[key].name);
+        }
+    );
+}
+container.forEach(function(f){
+    f();
+});
