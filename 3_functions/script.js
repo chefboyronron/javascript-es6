af7d90b65eac630cd5c6caccf3a81538e3df0f83
chefboyronron <rgpseminiano@gmail.com>
2018-04-29 15:59:21 +0800

    add ES6 functions features

--- 3_functions/script.js
+++ 3_functions/script.js
@@ -74,3 +74,122 @@ function skipCount(multiplier = 1) {
 }
 var result = skipCount(3);
 console.log(result);
+
+// Spread parameters
+let array20 = [20, 30, 40, 50];
+console.log(Math.max(...array20));
+
+// add elements to array using spread params
+let parts  = ['shoulder', 'knees'];
+let lyrics = ['head', ...parts, 'and', 'toes'];
+console.log(lyrics);
+
+// add an element to array
+let new_lyrics = [...lyrics];
+new_lyrics.push('new element');
+console.log(lyrics, new_lyrics);
+
+// concatenation an array either of two output the same value
+//let concatArray = parts.concat(array20);
+let concatArray = [...parts, ...array20];
+console.log(concatArray);
+
+// meta property
+function Person(name) {
+    //if( this instanceof Person ) {
+    if( typeof new.target !== 'undefined' ) {
+        this.name = name;
+    } else {
+        throw new Error('Must have new keyword in front of the person');
+    }
+}
+var newPerson = new Person('Ron');
+console.log(newPerson);
+/* var anotherPerson = Person( 'Kyle' );
+console.log(anotherPerson); */
+
+// block level function
+// "use strict";
+if(true) {
+    function blockFunc() {
+        console.log('blockFunc');
+    }
+}
+
+// arrow function with one parameter
+var reflect = function(value) {
+    return value;
+}
+// arrow equivalent
+// var reflect = value => value;
+
+// arrow function with multiple parameters
+var isum = function (num1, num2) {
+    return num1 + num2;
+}
+// arrow equivalent
+// var isum = (num1, num2) => num1 + num2;
+
+// arrow function with no parameter
+var getName = function() {
+    return 'Ron';
+}
+// arrow equivalent
+// var getName = () => "Ron";
+
+// arrow function with validation
+var validate = function(num1, num2) {
+    if( typeof num1 === 'number' && typeof num2 === 'number' ){
+        console.log(typeof num1);
+    }
+}
+// arrow equivalent
+/* var validate = (num1, num2) => {
+    if( typeof num1 === 'number' && typeof num2 === 'number' ){
+        console.log(typeof num1);
+    }
+} */
+
+// arrow function return an object
+/* var objFunction = function(id){
+    return {
+        'id' : id,
+        'location' : 'Temp'
+    }
+} */
+// arrow equivalent
+// var objFunction = (id) => ({tempId: id, location: 'Temp'});
+
+// Example
+var myArrowObj = {
+    name : 'Ron',
+    age : 32
+};
+var objFunction = (name) => {
+    myArrowObj.name = name;
+    return myArrowObj;
+};
+console.log(objFunction('Kyle'));
+
+// arrow function sorting
+var arrowNums = [5,31,76,9,0,30];
+var arrowSortAsc = arrowNums.sort( (n1, n2) => n1 - n2 );
+console.log(arrowSortAsc);
+
+// arrow function with no argument and passing parameters :(
+var noArgument = function() {
+    return () => arguments
+}
+var noArgumentCall = noArgument('ron','gerald');
+console.log(noArgumentCall());
+
+// arrow function with IIFE's (immediately invoked function expressions)
+let iifePerson = ((name) => {
+    return {
+        getTheName : function() {
+            return name;
+        }
+    }
+})(name);
+
+console.log(iifePerson.getTheName('Ron'));
